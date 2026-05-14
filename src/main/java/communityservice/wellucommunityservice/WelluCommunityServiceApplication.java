b3eefac14d6344bc9e46296e91df9ad91e47f8d2
Rola Hadidy <rolatotta2004@gmail.com>
2026-05-14 23:20:29 +0300

    initial commit

---
+++ src/main/java/communityservice/wellucommunityservice/WelluCommunityServiceApplication.java
@@ -0,0 +1,13 @@
+package communityservice.wellucommunityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelluCommunityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WelluCommunityServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/communityservice/wellucommunityservice/WelluCommunityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package communityservice.wellucommunityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WelluCommunityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
