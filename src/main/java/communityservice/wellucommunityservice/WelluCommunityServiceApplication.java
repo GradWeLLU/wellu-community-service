0df90bebf4baf0efc29c36d2fc1cab62cf2b9714
Rola Hadidy <rolatotta2004@gmail.com>
2026-05-14 23:24:50 +0300

    add pom.xml

--- src/main/java/communityservice/wellucommunityservice/WelluCommunityServiceApplication.java
+++ src/main/java/communityservice/wellucommunityservice/WelluCommunityServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WelluCommunityServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WelluCommunityServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WelluCommunityServiceApplication.class, args);
+    }
 
 }
--- src/test/java/communityservice/wellucommunityservice/WelluCommunityServiceApplicationTests.java
+++ src/test/java/communityservice/wellucommunityservice/WelluCommunityServiceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class WelluCommunityServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
